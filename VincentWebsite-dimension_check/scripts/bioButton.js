6d6eeff21c6d1fa6ed45aabe9709bfd0464f7257
Derek Wu <derekwu114@gmail.com>
2020-09-08 03:32:49 -0400

    Fixed incorrect merge

--- VincentWebsite-dimension_check/scripts/bioButton.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#bbut").click(function(){
-        $("[type = contents]").fadeOut();
-        setTimeout(function(){location.href = "bio.html";}, 500);
-    });
-})
